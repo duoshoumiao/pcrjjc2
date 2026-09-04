527b05ebe14cd9a89915c7cf227199d9800af45b
duoshoumiao <94856184+duoshoumiao@users.noreply.github.com>
2026-09-04 13:15:47 +0800

    更新

--- __init__.py
+++ __init__.py
@@ -600,7 +600,7 @@ async def no_private(bot: HoshinoBot, ev: CQEvent):
 @on_command('del_binds', aliases=('pcrjjc删除绑定', '渠pcrjjc删除绑定', '台pcrjjc删除绑定'), only_to_me=False)
 async def del_binds(session: NoticeSession):
    # 检查是否为超级用户
-    if not session.ctx.user_id in SUPERUSERS:
+    if not session.ctx.user_id in ADMIN:
         return
     # 获取命令参数（QQ号）
     qq_num = session.current_arg_text.strip()
